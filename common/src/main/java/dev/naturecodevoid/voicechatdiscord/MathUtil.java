dc845ee89923fcd7751b1a4732a61ba9d34e2f4b
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2023-01-27 21:49:44 -0800

    add public to mathutil methods

--- common/src/main/java/dev/naturecodevoid/voicechatdiscord/MathUtil.java
+++ common/src/main/java/dev/naturecodevoid/voicechatdiscord/MathUtil.java
@@ -3,11 +3,11 @@ package dev.naturecodevoid.voicechatdiscord;
 import de.maxhenkel.voicechat.api.Position;
 
 public class MathUtil {
-    static double clamp(double val, double min, double max) {
+    public static double clamp(double val, double min, double max) {
         return Math.min(max, Math.max(min, val));
     }
 
-    static double distance(Position pos1, Position pos2) {
+    public static double distance(Position pos1, Position pos2) {
         return Math.sqrt(
                 Math.pow(pos1.getX() - pos2.getX(), 2) +
                         Math.pow(pos1.getY() - pos2.getY(), 2) +
